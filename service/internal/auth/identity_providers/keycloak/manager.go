41fac2af10b3651235b3774124652713e16d2967
Alex Shevchuk <shevchuk.aa@phystech.edu>
2025-02-26 14:27:03 +0300

    SUBSMANAGER-21 added draft google auth

---
+++ service/internal/auth/identity_providers/keycloak/manager.go
@@ -0,0 +1,75 @@
+package idpmanager
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	idprovider "github.com/AlexOreL-272/Subscription-Tracker/internal/auth/identity_providers"
+	"github.com/AlexOreL-272/Subscription-Tracker/internal/domain"
+	"golang.org/x/oauth2"
+)
+
+type KeycloakOauthConfig struct {
+	oauthCfg         *oauth2.Config
+	userInfoEndpoint string
+	providers        []idprovider.IdentityProvider
+}
+
+func New(
+	cfg *oauth2.Config,
+	userInfoEndpoint string,
+	providers ...idprovider.IdentityProvider,
+) *KeycloakOauthConfig {
+	return &KeycloakOauthConfig{
+		cfg,
+		userInfoEndpoint,
+		providers,
+	}
+}
+
+func (k *KeycloakOauthConfig) GetRedirectURL(providerType idprovider.IdProviderType) (string, error) {
+	// TODO: add state generation
+	switch providerType {
+	case idprovider.YandexIdProvider:
+		return k.oauthCfg.AuthCodeURL(
+			"",
+			oauth2.SetAuthURLParam("kc_idp_hint", "yandex"),
+		), nil
+
+	case idprovider.GoogleIdProvider:
+		return k.oauthCfg.AuthCodeURL(
+			"",
+			oauth2.SetAuthURLParam("kc_idp_hint", "google"),
+		), nil
+
+	default:
+		return "", idprovider.ErrUnknownProvider
+	}
+}
+
+func (k *KeycloakOauthConfig) HandleIdPCallback(
+	code string,
+) (*domain.UserInfo, error) {
+	const op = "idprovider.HandleIdPCallback"
+	// TODO: add state check
+
+	token, err := k.oauthCfg.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	client := k.oauthCfg.Client(context.Background(), token)
+
+	userInfo, err := client.Get(k.userInfoEndpoint)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	var user domain.UserInfo
+	if err := json.NewDecoder(userInfo.Body).Decode(&user); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &user, nil
+}
--- service/internal/http/handlers.go
+++ service/internal/http/handlers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/AlexOreL-272/Subscription-Tracker/internal/auth"
+	idprovider "github.com/AlexOreL-272/Subscription-Tracker/internal/auth/identity_providers"
 	yandexauth "github.com/AlexOreL-272/Subscription-Tracker/internal/auth/identity_providers/yandex"
 	"github.com/AlexOreL-272/Subscription-Tracker/internal/domain"
 	"github.com/AlexOreL-272/Subscription-Tracker/internal/storage"
@@ -17,6 +18,7 @@ import (
 type Handler struct {
 	logger      *zap.Logger
 	authClient  auth.Auth
+	idPManager  idprovider.IdentityProviderManager
 	userSaver   storage.UserSaver
 	subSaver    storage.SubscriptionSaver
 	subProvider storage.SubscriptionProvider
@@ -29,6 +31,7 @@ type Handler struct {
 
 func New(
 	authClient auth.Auth,
+	idPManager idprovider.IdentityProviderManager,
 	userSaver storage.UserSaver,
 	subSaver storage.SubscriptionSaver,
 	subProvider storage.SubscriptionProvider,
@@ -167,6 +170,40 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
+}
+
+func (h *Handler) AuthWithIdentityProvider(w http.ResponseWriter, r *http.Request) {
+	const handler = "http.Handler.AuthWithIdentityProvider"
+
+	provider := r.FormValue("provider")
+
+	url, err := h.idPManager.GetRedirectURL(idprovider.IdProviderType(provider))
+	if err != nil {
+		h.logger.
+			With(zap.String("operation", handler)).
+			Error("failed to get redirect url", zap.Error(err))
+		http.Error(w, fmt.Sprintf("unknown provider %q", provider), http.StatusBadRequest)
+
+		return
+	}
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func (h *Handler) AuthCallback(w http.ResponseWriter, r *http.Request) {
+	const handler = "http.Handler.AuthCallback"
+
+	user, err := h.idPManager.HandleIdPCallback(r.FormValue("code"))
+	if err != nil {
+		h.logger.
+			With(zap.String("operation", handler)).
+			Error("failed to handle idp callback", zap.Error(err))
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+
+		return
+	}
+
+	fmt.Printf("%+v\n", user)
 }
 
 func (h *Handler) LoginWithYandex(w http.ResponseWriter, r *http.Request) {
