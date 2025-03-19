90a93798ce2ed2e74c14341622f2352fc881cbef
Alex Shevchuk <shevchuk.aa@phystech.edu>
2025-03-20 00:32:03 +0300

    SUBSMANAGER-YYY modified reset password and static file manager

--- service/internal/http/static.go
+++ service/internal/http/static.go
@@ -24,19 +24,19 @@ func (s *StaticFileHandler) ServeVerifyEmailPage(w http.ResponseWriter, r *http.
 }
 
 func (s *StaticFileHandler) ServeSuccessVerificationPage(w http.ResponseWriter, r *http.Request) {
-	confirmationFilePath := filepath.Join(s.templatesPath, "success_verification", "success_verification.html")
+	confirmedFilePath := filepath.Join(s.templatesPath, "verify_email", "success_verification.html")
 
-	http.ServeFile(w, r, confirmationFilePath)
+	http.ServeFile(w, r, confirmedFilePath)
 }
 
 func (s *StaticFileHandler) ServeForgotPasswordPage(w http.ResponseWriter, r *http.Request) {
-	confirmationFilePath := filepath.Join(s.templatesPath, "reset_password", "reset_password.html")
+	resetPswFilePath := filepath.Join(s.templatesPath, "reset_password", "reset_password.html")
 
-	http.ServeFile(w, r, confirmationFilePath)
+	http.ServeFile(w, r, resetPswFilePath)
 }
 
 func (s *StaticFileHandler) ServeSuccessResetPage(w http.ResponseWriter, r *http.Request) {
-	confirmationFilePath := filepath.Join(s.templatesPath, "reset_password", "success_reset.html")
+	successResetPswFilePath := filepath.Join(s.templatesPath, "reset_password", "success_reset.html")
 
-	http.ServeFile(w, r, confirmationFilePath)
+	http.ServeFile(w, r, successResetPswFilePath)
 }
